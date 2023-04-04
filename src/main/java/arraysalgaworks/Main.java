ecdcd57b9ab320e3b46470449ade80a306c500ee
José Ronaldo <dev.joseronaldo@gmail.com>
2023-04-04 12:03:03 -0300

    feat: arrays: declaration forms

---
+++ src/main/java/arraysalgaworks/Main.java
@@ -0,0 +1,11 @@
+package arraysalgaworks;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] notas = new int[5];
+
+        int[] notas2 = new int[]{8, 5, 4, 9, 10};
+
+        int [] notas3 = {5, 10, 2, 9, 4};
+    }
+}
