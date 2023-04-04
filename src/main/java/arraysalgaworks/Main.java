aaf6f91236fd45b5f207a61f21129e1bcc9afe58
José Ronaldo <dev.joseronaldo@gmail.com>
2023-04-04 12:22:50 -0300

    feat: iterating arrays

--- src/main/java/arraysalgaworks/Main.java
+++ src/main/java/arraysalgaworks/Main.java
@@ -2,10 +2,13 @@ package arraysalgaworks;
 
 public class Main {
     public static void main(String[] args) {
-        int[] notas = new int[5];
+//        int[] notas3 = new int[5];
+//        int[] notas2 = new int[]{8, 5, 4, 9, 10};
 
-        int[] notas2 = new int[]{8, 5, 4, 9, 10};
+        int [] notas = {5, 10, 1, 9, 4};
 
-        int [] notas3 = {5, 10, 2, 9, 4};
+        System.out.println(Calculadora.calcularMedia(notas));
+
+
     }
 }
