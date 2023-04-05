0a6c28f769cb8a4c1e2462e3e3df59faa0673ea7
José Ronaldo <dev.joseronaldo@gmail.com>
2023-04-05 16:50:58 -0300

    feat: exercício 1 polimorfismo

---
+++ src/main/java/exerciciopolimorfismo1/entites/Funcionario.java
@@ -0,0 +1,44 @@
+package exerciciopolimorfismo1.entites;
+
+public class Funcionario {
+    private String name;
+    private Integer horasTrabalhadas;
+    private Double valorPorHora;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String name, Integer horasTrabalhadas, Double valorPorHora) {
+        this.name = name;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorPorHora = valorPorHora;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(Integer horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public Double getValorPorHora() {
+        return valorPorHora;
+    }
+
+    public void setValorPorHora(Double valorPorHora) {
+        this.valorPorHora = valorPorHora;
+    }
+
+    public Double pagamento() {
+        return this.valorPorHora * horasTrabalhadas;
+    }
+}
