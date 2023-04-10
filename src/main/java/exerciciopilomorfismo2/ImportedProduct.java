43b5efa7a30b7ab8aa9d4944334e9ef52a20f882
José Ronaldo <dev.joseronaldo@gmail.com>
2023-04-10 11:18:33 -0300

    feat: exercício 2 polimorfismo

---
+++ src/main/java/exerciciopilomorfismo2/ImportedProduct.java
@@ -0,0 +1,28 @@
+package exerciciopilomorfismo2;
+
+public class ImportedProduct extends Product {
+    private Double customsFee;
+
+    public ImportedProduct(String name, double price, double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public Double totalPRice() {
+    return getPrice() + customsFee;
+    }
+
+    @Override
+    public final String priceTag() {
+            return super.priceTag() + " (Customs fee: $ " + String.format("%.2f", customsFee) + ")";
+
+    }
+}
